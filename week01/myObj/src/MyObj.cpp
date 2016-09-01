136f7da73d8b19bef1c4e429af53af387b5588ff
Regina Flores <florr422@newschool.edu>
2016-09-01 15:02:53 -0400

    notes

---
+++ week01/myObj/src/MyObj.cpp
@@ -0,0 +1,9 @@
+//
+//  MyObj.cpp
+//  myObj
+//
+//  Created by Regina Flores on 9/1/16.
+//
+//
+
+#include "MyObj.hpp"
---
+++ week01/myObj/src/MyObj.hpp
@@ -0,0 +1,14 @@
+//
+//  MyObj.hpp
+//  myObj
+//
+//  Created by Regina Flores on 9/1/16.
+//
+//
+
+#ifndef MyObj_hpp
+#define MyObj_hpp
+
+#include <stdio.h>
+
+#endif /* MyObj_hpp */
